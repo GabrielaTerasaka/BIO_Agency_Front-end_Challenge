9d1a8049cc4fb7c74f5c1bc6088bb639a1838840
Gabriela Terasaka <gabriela.terasaka@gmail.com>
2022-01-10 15:19:27 -0500

    adding react-persist library}

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={configureStore()}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
