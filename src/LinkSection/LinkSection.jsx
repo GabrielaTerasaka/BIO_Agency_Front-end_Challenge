e048d6b04dfa573814042a8b47fb988ed6aba7e3
Gabriela Terasaka <gabriela.terasaka@gmail.com>
2022-01-10 16:25:10 -0500

    update styles of components

--- src/LinkSection/LinkSection.jsx
+++ src/LinkSection/LinkSection.jsx
@@ -9,24 +9,27 @@ const LinkSection = ({ links, message }) => {
   return (
     <section className="link-section">
       <LinkForm />
-      {message && (
-        <p className="link-section__message">
-          {message === "true"
-            ? "Success!"
-            : "Fail! Either the URL is empty or invalid"}
-        </p>
-      )}
-      {links.length === 0 && (
-        <div className="link-section__wrapper">
-          <h2 className="link-section">Advanced Statistics</h2>
-          <p className="link-section__text">
-            Track how your links are performing across the web with our advanced
-            statistics dashboard
+      <div className="link-section__wrapper">
+        {message && (
+          <p className="link-section__message">
+            {message === "true"
+              ? "Success!"
+              : "Fail! Either the URL is empty or invalid"}
           </p>
-        </div>
-      )}
-      {links.length > 0 &&
-        [...links].reverse().map((link) => <LinkDisplay link={link} />)}
+        )}
+
+        {links.length === 0 && (
+          <>
+            <h2 className="link-section">Advanced Statistics</h2>
+            <p className="link-section__text">
+              Track how your links are performing across the web with our
+              advanced statistics dashboard
+            </p>
+          </>
+        )}
+        {links.length > 0 &&
+          [...links].reverse().map((link) => <LinkDisplay link={link} />)}
+      </div>
     </section>
   );
 };
